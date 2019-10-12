b7908e4112c158ad4cc85952974e41524cbe87b1
Nico Pellerin <hello@nicopellerin.io>
2019-10-12 15:24:53 -0400

    Changes VR scene

--- src/components/room.js
+++ src/components/room.js
@@ -7,15 +7,15 @@ const Room = ({ show }) => {
   const [model, setModel] = useState()
 
   useEffect(() => {
-    new GLTFLoader().load("/new/scene.gltf", setModel)
+    new GLTFLoader().load("/other/scene.gltf", setModel)
   }, [])
 
   return model ? (
     <primitive
       visible={show}
-      scale={[1, 1, 1]}
+      scale={[0.8, 0.8, 0.8]}
       object={model.scene}
-      position={[6, -70, 9.3]}
+      position={[280, -15, -160.3]}
       rotation={[-Math.PI / 2, Math.PI / 2, Math.PI / 2]}
     />
   ) : null
