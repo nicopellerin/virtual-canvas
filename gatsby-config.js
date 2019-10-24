b00b41d7ff0afdc958f293e92d6217842c3ce239
Nico Pellerin <hello@nicopellerin.io>
2019-10-24 19:28:58 -0400

    Remove service worker

--- gatsby-config.js
+++ gatsby-config.js
@@ -41,6 +41,6 @@ module.exports = {
     `gatsby-plugin-typescript`,
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
-    `gatsby-plugin-offline`,
+    // `gatsby-plugin-offline`,
   ],
 }
