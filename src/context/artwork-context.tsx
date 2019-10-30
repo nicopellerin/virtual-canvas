84b670fbac1b8b17ae64e33ab0d69b19f7047069
Nico Pellerin <hello@nicopellerin.io>
2019-10-29 21:08:44 -0400

    adjust default light intensity

--- src/context/artwork-context.tsx
+++ src/context/artwork-context.tsx
@@ -9,7 +9,7 @@ interface Props {
 export const ArtworkProvider = ({ children }: Props) => {
   const [checkedBackground, setCheckedBackground] = useState<boolean>(false)
   const [rotateCanvas, setRotateCanvas] = useState<boolean>(true)
-  const [lightIntensity, setLightIntensity] = useState<number>(4)
+  const [lightIntensity, setLightIntensity] = useState<number>(1)
   const [showTexture, setShowTexture] = useState<boolean>(true)
   const [showBorder, setShowBorder] = useState<boolean>(true)
   const [artworkName, setArtworkName] = useState<string>("")
