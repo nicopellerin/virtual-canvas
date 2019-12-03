a83cb64506bde42968c093e3c8ae602493e97aff
Nico Pellerin <hello@nicopellerin.io>
2019-12-02 20:49:27 -0500

    New landing page2

--- src/components/landing/main.tsx
+++ src/components/landing/main.tsx
@@ -115,7 +115,11 @@ const Main = () => {
         <motion.img
           src={Back}
           alt="Background"
-          style={{ borderRadius: 5, boxShadow: '0 0 15px rgba(0,0,0,0.3)' }}
+          style={{
+            borderRadius: 5,
+            boxShadow: '0 0 15px rgba(0,0,0,0.3)',
+            opacity: 0,
+          }}
           width={600}
           animate={{ opacity: [0, 1], y: [20, 0], transition: { delay: 0.3 } }}
         />
